59b44db46d0f6bb39538e9cce2785a24406026b8
jluisreyvargas <jluisreyvargas@gmail.com>
2026-01-10 17:20:33 +0100

    Updated

--- infra/terraform/bootstrap/variables.tf
+++ infra/terraform/bootstrap/variables.tf
@@ -1,4 +1,18 @@
-variable "aws_region" { type = string }
-variable "project_name" { type = string  default = "listmonk2" }
-variable "environment" { type = string  default = "dev" }
-variable "owner" { type = string default = "devops-team" }
+variable "aws_region" {
+  type = string
+}
+
+variable "project_name" {
+  type    = string
+  default = "listmonk2"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "owner" {
+  type    = string
+  default = "devops-team"
+}
